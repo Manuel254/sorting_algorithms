ff79d97fe6037eea6e8334dfe4ca50b43a962926
Manuel254 <immanuelkituku@gmail.com>
2023-01-24 09:09:08 -0800

    Completed

---
+++ 0-bubble_sort.c
@@ -0,0 +1,39 @@
+#include "sort.h"
+
+/**
+ * swap - swaps two numbers
+ * @num_1: pointer to first number
+ * @num_2: pointer to second number
+ */
+void swap(int *num_1, int *num_2)
+{
+	int temp = *num_1;
+	*num_1 = *num_2;
+	*num_2 = temp;
+}
+
+/**
+ * bubble_sort - sorts an array using the bubble sort algorithm
+ * @array: array to be sorted
+ * @size: size of array
+ */
+void bubble_sort(int *array, size_t size)
+{
+	size_t i, j, flag;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		flag = 0;
+		for (j = 0; j < size - i - 1; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				swap(&array[j], &array[j + 1]);
+				print_array(array, size);
+				flag = 1;
+			}
+		}
+		if (flag == 0)
+			break;
+	}
+}
